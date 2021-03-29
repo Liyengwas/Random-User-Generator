974ce45d0aa607cd5ef1370fc609d62da6cbb4ae
Liyengwas <liyengwas@gmail.com>
2021-03-29 15:00:57 +0300

    api integration into app.js file

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 const app = Vue.createApp({
-    template: '<h1>Hello World ;)</h1>'
-})
+  data() {
+    return {
+      firstName: "Solomon",
+      lastName: "Eagle",
+      email: "liyengwas@gmail.com",
+      gender: "male",
+      picture: "https://randomuser.me/api/portraits/men/10.jpg",
+    };
+  },
 
+  methods: {
+    async generateUser() {
+      const res = await fetch("https://randomuser.me/api");
+      const { results } = await res.json();
 
-app.mount('#app');
+      console.log("request completed successfully",results);
+
+      this.firstName = results[0].name.first;
+      this.lastName = results[0].name.last;
+      this.email = results[0].email;
+      this.gender = results[0].gender;
+      this.picture = results[0].picture.large;
+    },
+  },
+});
+
+app.mount("#app");
